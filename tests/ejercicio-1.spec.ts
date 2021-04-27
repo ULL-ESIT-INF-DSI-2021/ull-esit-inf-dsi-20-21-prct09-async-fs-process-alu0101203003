d76f2e32a292553ff2d3226ae83080dabbafa0bd
alu0101203003 <alu0101203003@ull.edu.es>
2021-04-27 17:14:51 +0100

    Estructura inicial creada

---
+++ tests/ejercicio-1.spec.ts
@@ -0,0 +1,16 @@
+/*
+import 'mocha';
+import {expect} from 'chai';
+import {nombreFuncion} from '../src/ejercicio-1';
+
+describe('Ejercicio 1', () => {
+    it('nombreFuncion(argumentos) returns valor', () => {
+      expect(nombreFuncion(argumentos)).to.be.deep.equal(valor);
+    });
+    
+    it('nombreFuncion(argumentos) returns valor', () => {
+      expect(nombreFuncion(argumentos)).to.be.deep.equal(valor);
+    });
+
+});
+*/
